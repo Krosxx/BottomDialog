f5ea9613ce8a3f1b2ff751171f44de1e0b63dd42
Vove <1132412166@qq.com>
2019-08-05 16:25:37 +0800

    修改Awesome little_title样式

--- extension/src/main/java/cn/vove7/bottomdialog/extension/AwesomeHeader.kt
+++ extension/src/main/java/cn/vove7/bottomdialog/extension/AwesomeHeader.kt
@@ -14,7 +14,7 @@ import cn.vove7.bottomdialog.interfaces.ContentBuilder
 import cn.vove7.bottomdialog.util.fadeIn
 import cn.vove7.bottomdialog.util.fadeOut
 import cn.vove7.bottomdialog.util.listenToUpdate
-import kotlinx.android.synthetic.main.header_awsome.view.*
+import kotlinx.android.synthetic.main.header_awesome.view.*
 
 
 fun BottomDialogBuilder.awesomeHeader(title: String) {
@@ -31,7 +31,7 @@ fun BottomDialogBuilder.awesomeHeader(title: String) {
  */
 class AwesomeHeader : ContentBuilder(), StatusCallback {
     override val layoutRes: Int
-        get() = R.layout.header_awsome
+        get() = R.layout.header_awesome
 
     var title: String? by listenToUpdate(null, this)
 
@@ -112,12 +112,13 @@ class AwesomeHeader : ContentBuilder(), StatusCallback {
      */
     private fun setStatusbarColor() {
 
-        val f = dialog.window.decorView.systemUiVisibility
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             if (status == 0) {
-                dialog.window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                dialog.window?.decorView?.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             } else {
-                dialog.window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    dialog.window?.decorView?.systemUiVisibility = SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                }
             }
         }
     }
