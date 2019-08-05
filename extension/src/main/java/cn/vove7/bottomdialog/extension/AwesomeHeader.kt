a8d326345e079d4f54ed0d53ea3cdfee2ce4d049
Vove <1132412166@qq.com>
2019-08-05 16:26:53 +0800

    修改Awesome little_title样式

--- extension/src/main/java/cn/vove7/bottomdialog/extension/AwesomeHeader.kt
+++ extension/src/main/java/cn/vove7/bottomdialog/extension/AwesomeHeader.kt
@@ -112,12 +112,13 @@ class AwesomeHeader : ContentBuilder(), StatusCallback {
      */
     private fun setStatusbarColor() {
 
-        val f = dialog.window.decorView.systemUiVisibility
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             if (status == 0) {
-                dialog.window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                dialog.window?.decorView?.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             } else {
-                dialog.window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    dialog.window?.decorView?.systemUiVisibility = SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                }
             }
         }
     }
